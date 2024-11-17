72fcc56f5a91c7f3cf61a0087b60cd17f3067c5e
bradcodecraft <bradcodecraft@gmail.com>
2024-11-16 19:08:47 -0500

    nvim

---
+++ lua/config/settings.lua
@@ -0,0 +1,16 @@
+-- Disable nerd font
+vim.g.have_nerd_font = false
+
+-- Make line numbers default
+vim.opt.number = true
+
+-- Make <tab> 2 character wide
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Show cursor line
+vim.opt.cursorline = true
--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,3 +1,28 @@
+-- Change colorscheme
+function ColorMyPencils(color)
+	color = color or "catppuccin"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 return {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "macchiato",
+				transparent_background = true,
+				background = {
+					dark = "mocha",
+				},
+			})
+
+			vim.cmd.colorscheme("catppuccin")
+			ColorMyPencils()
+		end,
+	},
 }
