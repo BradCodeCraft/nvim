306219bebb8f16df0b1fb21692c2fa16491513c3
bradcodecraft <bradcodecraft@gmail.com>
2024-12-06 10:53:44 -0500

    formatting changes

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,5 +1,5 @@
 local config = {
-    cmd = {"C:/Program Files/Java/jdk-23/bin/server"}, -- Change this to your JDK path
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+	cmd = { "C:/Program Files/Java/jdk-23/bin/server" }, -- Change this to your JDK path
+	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
 }
-require('jdtls').start_or_attach(config)
+require("jdtls").start_or_attach(config)
