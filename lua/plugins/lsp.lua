2d6cd42e7728ef1ac37ffd322bdf4e2827b5dce7
bradcodecraft <bradcodecraft@gmail.com>
2024-12-06 09:27:19 -0500

    0.0.15: jdtls server feature added

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -172,7 +172,7 @@ return {
 			--
 			-- But for many setups, the LSP (`ts_ls`) will work just fine
 			ts_ls = {},
-			jdtls = {},
+			-- jdtls = {},
 			html = {},
 			cssls = {},
 			jsonls = {},
