3e4abbdc0b522b4a2ac8e5b969f6cee73a2d8b79
BradCodeCraft <bradcodecraft@gmail.com>
2024-12-26 19:14:22 -0500

    0.0.18: undotree feature updated (OS flexible)

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -4,6 +4,11 @@ return {
 		if vim.fn.has("win32") then
 			vim.g.undotree_DiffCommand = "FC" -- required for windows machine
 		end
+
+		if vim.fn.has("macunix") then
+			vim.g.undotree_DiffCommand = "diff"
+		end
+
 		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undotree Toggle" })
 	end,
 }
