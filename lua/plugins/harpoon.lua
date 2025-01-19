9aabaa949a023c3c5e6b0baf6eb0d7f7f6c7ce9a
bradcodecraft <bradcodecraft@gmail.com>
2025-01-19 03:47:27 -0500

    add harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,23 @@
+return { -- NOTE: harpoon
+	"theprimeagen/harpoon",
+	config = function()
+		local mark = require("harpoon.mark")
+		local ui = require("harpoon.ui")
+
+		vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon" })
+		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Toogle Harpoon Menu" })
+
+		vim.keymap.set("n", "<C-h>", function()
+			ui.nav_file(1)
+		end)
+		vim.keymap.set("n", "<C-t>", function()
+			ui.nav_file(2)
+		end)
+		vim.keymap.set("n", "<C-n>", function()
+			ui.nav_file(3)
+		end)
+		vim.keymap.set("n", "<C-s>", function()
+			ui.nav_file(4)
+		end)
+	end,
+}
