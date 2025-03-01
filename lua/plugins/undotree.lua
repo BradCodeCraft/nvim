67bb62a755d37cd165baba6b71dc7e298067d109
bradcodecraft <bradcodecraft@gmail.com>
2025-03-01 18:20:17 -0500

    code format

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     -- TODO: make sure undotree works in Windows
 
-    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = "Show undotree" });
     vim.g.undotree_WindowLayout = 3;
     vim.g.undotree_SplitWidth = 30;
   end
